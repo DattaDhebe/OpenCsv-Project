ad1e551205ca3c30dfa9f751a49960d6936e69c7
DattaDhebe <dattadhebe75@gmail.com>
2020-05-07 09:04:28 +0000

    Added. 'useCase1' Reading Records One By One.

--- src/main/java/com/opencsv/OpenCSVReader.java
+++ src/main/java/com/opencsv/OpenCSVReader.java
@@ -8,12 +8,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class OpenCSVReader {
-    private static final String SAMPLE_CSV_FILE_PATH = "/home/datta/Desktop/Intellige Projects/OpenCSV Dependency/src/main/java/com/opencsv/sample.csv";
+    private static final String SAMPLE_CSV_FILE_PATH = "/home/datta/Desktop/Intellige Projects/OpenCSV Dependency/src/main/resources/sample.csv";
     public static void main(String[] args) throws IOException {
         try (
             Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
             CSVReader csvReader = new CSVReader(reader);
         ) {
+            //reading Record one by one
             String[] nextRecord;
             while ( (nextRecord = csvReader.readNext()) != null) {
                 System.out.println("Name : " +nextRecord[0]);
