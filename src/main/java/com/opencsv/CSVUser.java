9910de9f51ed29502ab7a2b211f3f956e001b4d5
DattaDhebe <dattadhebe75@gmail.com>
2020-05-07 21:54:08 +0000

    Added. 'useCase3' main program to parse CSV into POJO class.

---
+++ src/main/java/com/opencsv/CSVUser.java
@@ -0,0 +1,43 @@
+package com.opencsv;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVUser {
+    @CsvBindByName
+    private String name;
+
+    @CsvBindByName(column = "email", required = true)
+    private  String email;
+
+    @CsvBindByName(column = "phone")
+    private String phoneNo;
+
+    @CsvBindByName
+    private String country;
+
+    @Override
+    public  String toString() {
+        return "CSVUserTest{" +
+                "name=" + name + '\'' +
+                ", email=" + email + '\'' +
+                ", phoneNo=" + phoneNo + '\'' +
+                ", country=" + country + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
---
+++ src/main/java/com/opencsv/OpenCSVReadAndParseToBean.java
@@ -0,0 +1,40 @@
+package com.opencsv;
+
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.File;
+import java.util.*;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class OpenCSVReadAndParseToBean {
+    private static final String SAMPLE_CSV_FILE_PATH = "/home/datta/Desktop/Intellige Projects/OpenCSV Dependency/src/main/resources/sample.csv";
+
+    public static void main(String[] args) throws IOException {
+        try (
+            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+        ) {
+            CsvToBean<CSVUser> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CSVUser.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
+
+            while (csvUserIterator.hasNext()) {
+                CSVUser csvUser = csvUserIterator.next();
+                System.out.println("Name : " +csvUser.getName());
+                System.out.println("Email : " +csvUser.getEmail());
+                System.out.println("Phone : " +csvUser.getPhoneNo());
+                System.out.println("Country : " +csvUser.getCountry());
+                System.out.println("============================");
+            }
+        }
+    }
+}
+
+
